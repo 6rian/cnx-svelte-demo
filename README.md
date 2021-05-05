b983d50d2d6fa5a20ab12bd71e4763271d9a0b35
Brian Griffin <wbgriffin@gmail.com>
2021-05-05 10:37:20 -0400

    Added optional query params to force player/lineitem/video

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ npm run dev
 ```
 
 Navigate to [localhost:5000](http://localhost:5000).
+
+Optional Query Params:
+
+- `playerId`
+- `lineItemId`
+- `videoType=(vast|hls)`
+- `videoUrl`
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script>
   import Player from './Player.svelte'
+
+  const query = new URLSearchParams(window.location.search);
+  const params = {
+    playerId: 'f77f1ffc-2602-45e7-a41e-68e65b9bd20b',
+    lineItemId: 'e642cb74-0713-44f1-b858-8348ae737abb',
+    videoUrl: 'https://assets.connatix.com/Elements/a2b4e744-6884-4300-8eeb-a4394fe485d4/Disney-Vast.xml',
+    videoType: 'vast'
+  };
+
+  Object.keys(params).forEach(key => {
+    if (query.has(key) && query.get(key) !== '') {
+      params[key] = query.get(key);
+    }
+  });
 </script>
 
 <style>
@@ -15,9 +29,9 @@
     customParam1="customParam1"
     customParam2="customParam2"
     customParam3="customParam3"
-    videoUrl="https://assets.connatix.com/Elements/a2b4e744-6884-4300-8eeb-a4394fe485d4/Disney-Vast.xml"
-    videoType="vast"
-    cnxPlayerId="f77f1ffc-2602-45e7-a41e-68e65b9bd20b"
-    cnxLineItemId="e642cb74-0713-44f1-b858-8348ae737abb"
+    videoUrl={params.videoUrl}
+    videoType={params.videoType}
+    cnxPlayerId={params.playerId}
+    cnxLineItemId={params.lineItemId}
   />
 </div>
